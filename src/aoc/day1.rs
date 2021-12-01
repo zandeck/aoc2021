0c80989fbc69ca2a43fc64ea0a7786b55ecc00ed
Matthieu <matthieu.zandecki@gmail.com>
2021-12-01 10:26:49 +0000

    refactor to use iterators

--- src/aoc/day1.rs
+++ src/aoc/day1.rs
@@ -3,38 +3,34 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use std::str::FromStr;
 
-fn read_data() -> io::Result<Vec<i32>> {
+fn read_data() -> io::Result<impl Iterator<Item = i32>> {
     let file = File::open("resources/day1.txt")?;
     let reader = BufReader::new(file);
 
-    reader
-        .lines()
-        .map_ok(|v| i32::from_str(&v).unwrap())
-        .collect()
+    Ok(reader.lines().map(|v| match v {
+        Ok(s) => i32::from_str(&s).unwrap(),
+        Err(_) => panic!("Error reading lines from file"),
+    }))
 }
 
-pub fn consecutive_increasing_depth(depths: &[i32]) -> usize {
-    depths.iter().tuple_windows().filter(|(a, b)| b > a).count()
+pub fn consecutive_increasing_depth(depths: impl Iterator<Item = i32>) -> usize {
+    depths.tuple_windows().filter(|(a, b)| b > a).count()
 }
 
-pub fn rolling_depth_3(depths: &[i32]) -> Vec<i32> {
-    depths
-        .iter()
-        .tuple_windows()
-        .map(|(a, b, c)| a + b + c)
-        .collect()
+pub fn rolling_depth_3(depths: impl Iterator<Item = i32>) -> impl Iterator<Item = i32> {
+    depths.tuple_windows().map(|(a, b, c)| a + b + c)
 }
 
 pub fn part1() -> io::Result<()> {
     let data = read_data()?;
-    println!("{}", consecutive_increasing_depth(&data));
+    println!("{}", consecutive_increasing_depth(data));
     Ok(())
 }
 
 pub fn part2() -> io::Result<()> {
     let data = read_data()?;
-    let rolling_window = rolling_depth_3(&data);
-    println!("{}", consecutive_increasing_depth(&rolling_window));
+    let rolling_window = rolling_depth_3(data);
+    println!("{}", consecutive_increasing_depth(rolling_window));
     Ok(())
 }
 
@@ -43,15 +39,14 @@ mod test {
     #[test]
     fn part1() {
         let depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(super::consecutive_increasing_depth(&depths), 7);
+        assert_eq!(super::consecutive_increasing_depth(depths.into_iter()), 7);
     }
 
     #[test]
     fn part2() {
         let depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        let rolling = super::rolling_depth_3(&depths);
-        let result = vec![607, 618, 618, 617, 647, 716, 769, 792];
-        assert_eq!(rolling, result);
-        assert_eq!(super::consecutive_increasing_depth(&rolling), 5);
+        let rolling = super::rolling_depth_3(depths.into_iter());
+        // let result = vec![607, 618, 618, 617, 647, 716, 769, 792];
+        assert_eq!(super::consecutive_increasing_depth(rolling), 5);
     }
 }
